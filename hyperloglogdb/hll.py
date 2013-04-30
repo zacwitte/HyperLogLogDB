977bf57c21bcecccad96f125a903c54e8a55ac18
Zac Witte <zacwitte@gmail.com>
2013-04-30 16:19:18 -0700

    added licensing information to all source files

--- hyperloglogdb/hll.py
+++ hyperloglogdb/hll.py
@@ -1,4 +1,21 @@
 """
+This file is part of HyperLogLogDB.
+
+HyperLogLogDB is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+HyperLogLogDB is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with HyperLogLogDB.  If not, see <http://www.gnu.org/licenses/>.
+
+----
+
 This module implements probabilistic data structure which is able to
 calculate the cardinality of large multisets in a single pass using
 little auxiliary memory.
